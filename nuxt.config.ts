71faa805626114f44a6b9ae31e9815f3de9f63c9
Andres Flores <info@andresflores.ca>
2023-09-17 18:03:03 -0400

    Fix random ass url formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,6 +37,8 @@ export default defineNuxtConfig({
         docSearch: {
           indexName: "netlify_5f628b0e-8bb6-41c7-ad1f-b941716df428_master_all",
         },
+        // @ts-ignore
+        transformItems: (items) => items,
       },
     },
   },
